9ce8559e0a9601b5b2ab6456ee0d2f1fdc1b3921
Yuristam <yuristamasanakunov@gmail.com>
2023-01-09 23:12:33 +0600

    updated chainsaw man

--- MatrixConsole/Chainsaw Man/ChainsawMan.cs
+++ MatrixConsole/Chainsaw Man/ChainsawMan.cs
@@ -7,6 +7,19 @@ namespace MatrixConsole
         //gun devil attack beginning
         internal static void GunDevil()
         {
+            //be careful
+            for (int i = 0; i < 10; i++)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.Clear();
+                string spoiler = "SPOILER ALERT";
+                Console.WriteLine(spoiler);
+                Thread.Sleep(1000);
+            }
+            Console.Clear();
+            Console.ResetColor();
+            //i warned you
+
             //for start typing machine sound
             if (OperatingSystem.IsWindows())
             {
--- MatrixConsole/Test/TestArea.cs
+++ MatrixConsole/Test/TestArea.cs
@@ -6,16 +6,35 @@ namespace MatrixConsole.Test
     {
         static internal void ShowTestArea()
         {
-
-            Console.OutputEncoding = Encoding.Unicode;
-            Console.InputEncoding = Encoding.Unicode;
-            Console.WriteLine("This is the Test Area.\r\n" +
+            while (true)
+            {
+                Console.WriteLine("This is the Test Area.\r\n" +
                 "Here you can try new learned things.");
+                Console.Write(">");
+                string input = Console.ReadLine();
 
-            Console.WriteLine("приключения");
-            string name = Console.ReadLine();
-            Console.WriteLine($"Список имен: matrix {name}");
-            Console.ReadKey();
+                if (input == "1")
+                {
+                    //the code below is the one for a Console to show sybols in different writing system (script)
+                    Console.OutputEncoding = Encoding.Unicode;
+                    Console.InputEncoding = Encoding.Unicode;
+                    //for example if there is a Cyrillic (кирилица) script
+                    Console.WriteLine("Какая-то надпись!!!");
+                    Console.ReadKey();
+                    //===========================================================================
+                }
+                else if (input == "get back" || input == "gb")
+                {
+                    Program.Main();
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Please, write something that is in the list above.");
+                    Thread.Sleep(1000);
+                    Console.Clear();
+                }
+            }
         }
     }
 }
